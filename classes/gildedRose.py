66eb4f14c9df574e3d08e57fac34448d2ec1b41a
benActor <210335@supinfo.com>
2020-01-26 17:36:12 +0100

    test update items

--- classes/gildedRose.py
+++ classes/gildedRose.py
@@ -1,14 +1,45 @@
 from datetime import datetime
 import re
+from classes.ItemService import ItemService
+from classes.sulfuraService import SulfuraService
+from classes.conjuredService import ConjuredService
+from classes.backStageService import BackstageService
+from classes.standardItemService import StandardItemService
+from classes.agedBrieService import AgedBrieService
 
 
 class GildedRose:
+    items = []
 
     @staticmethod
-    def get_time_in_right_format():
-        my_time = datetime.now().strftime('%H:%M:%S')
+    def time_in_right_format(my_time):
         if bool(re.match(r"\d\d:\d\d:\d\d", my_time)) and len(my_time) == 8:
             return True
         return False
+
+    @staticmethod
+    def is_end_of_the_day(my_time):
+        return GildedRose.time_in_right_format(my_time) and my_time == "00:00:00"
+
+    @staticmethod
+    def update_items(items):
+        if GildedRose.is_end_of_the_day(datetime.now().strftime('%H:%M:%S')):
+            for item in items:
+                if ItemService.is_item_conjured(item):
+                    ConjuredService.update_quality(item)
+                elif ItemService.is_item_back_stage(item):
+                    BackstageService.update_item_quality(item)
+                elif ItemService.is_item_sulfuras(item):
+                    SulfuraService.update_item(item)
+                elif ItemService.is_aged_brie(item):
+                    AgedBrieService.update_item(item)
+                elif not ItemService.is_special_item(item):
+                    StandardItemService.update_item_quality(item)
+
+
+if __name__ == '__main__':
+    items = []
+    GildedRose.update_items(items)
+
 
 
--- tests/test_gildedRose.py
+++ tests/test_gildedRose.py
@@ -4,11 +4,15 @@ from classes.gildedRose import GildedRose
 
 class test_GildedRose(unittest.TestCase):
     def test_can_get_time_in_right_format(self):
-        my_time = GildedRose.get_time_in_right_format()
-        self.assertTrue(my_time)
+        my_time = "00:23:45"
+        self.assertTrue(GildedRose.time_in_right_format(my_time))
 
     def test_can_get_end_of_the_day(self):
-        pass
+        my_time = "00:00:00"
+
+        self.assertTrue(GildedRose.is_end_of_the_day(my_time))
+
+        self.assertFalse(GildedRose.is_end_of_the_day("14:25:33"))
 
 
 if __name__ == '__main__':
