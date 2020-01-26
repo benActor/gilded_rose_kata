aa540c79f2182609e7890aa783b91e7f844c7a60
benActor <210335@supinfo.com>
2020-01-26 16:06:35 +0100

    can update sulfuras

--- classes/sulfuraService.py
+++ classes/sulfuraService.py
@@ -8,5 +8,8 @@ class SulfuraService:
             return item.quality
 
     @staticmethod
-    def update_item
+    def update_item(item):
+        SulfuraService.set_item_quality(item)
+        ItemService.reduce_item_sell_in(item)
+        return item.quality, item.sell_in
 
--- tests/test_sulfuraService.py
+++ tests/test_sulfuraService.py
@@ -12,7 +12,7 @@ class Test_Sulfuras_service(unittest.TestCase):
 
     def test_can_update_sulfura_item(self):
         item = Item("Sulfuras", 25, 50)
-        sell_in_expected = item.sell_in
+        sell_in_expected = item.sell_in - 1
         quality_expected = 80
 
         self.assertTrue(SulfuraService.update_item(item) == (quality_expected, sell_in_expected))
