168f0bae16bdc8b312cf57fdfbe0a1499e20a326
benActor <210335@supinfo.com>
2020-01-26 13:10:07 +0100

    able to reduce item sell_in

--- classes/ItemService.py
+++ classes/ItemService.py
@@ -1,10 +1,24 @@
 from classes.Item import Item
 
+
 class ItemService:
-    @ staticmethod
+    @staticmethod
     def is_item_valid(item):
         if type(item) is Item:
             return True
         raise TypeError("item should be of type item")
+
+    @staticmethod
+    def is_entry_value_integer(value):
+        if not type(value) is int:
+            raise TypeError("The value must be an integer")
+        return True
+
+    @staticmethod
+    def reduce_item_sell_in(item):
+        if ItemService.is_item_valid(item):
+            if ItemService.is_entry_value_integer(item.sell_in):
+                item.sell_in -= 1
+                return item.sell_in
 
 
--- tests/test_ItemService.py
+++ tests/test_ItemService.py
@@ -2,22 +2,29 @@ import unittest
 from classes.ItemService import ItemService
 from classes.Item import Item
 
+
 class TestItemService(unittest.TestCase):
 
     def test_can_detect_items_of_valid_type(self):
-
-        self.assertRaises(TypeError,  ItemService.is_item_valid, "item")
+        self.assertRaises(TypeError, ItemService.is_item_valid, "item")
 
         self.assertTrue(ItemService.is_item_valid, Item("Sulfuras", 20, 80))
 
+    # this test check if an entry is of type integer
+    def test_can_identify_valid_entry_value(self):
+        self.assertRaises(TypeError, ItemService.is_entry_value_integer, "a string")
+
+        self.assertTrue(ItemService.is_entry_value_integer, 2)
+
     def test_can_reduce_item_sell_in(self):
         item = Item("Sulfuras", 20, 80)
-        value = 1
-        expected_value = item.sell_in - value
-        self.assertTrue(ItemService.reduce_item_sell_in_by_value(item, value) == expected_value )
+        item2 = Item("AgedBrie", "20", 50)
 
+        expected_value = item.sell_in - 1
 
+        self.assertTrue(ItemService.reduce_item_sell_in(item) == expected_value)
 
+        self.assertRaises(TypeError, ItemService.reduce_item_sell_in, item2)
 
 
 
